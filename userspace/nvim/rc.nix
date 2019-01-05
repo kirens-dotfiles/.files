2d4158c2b3fdb2280089f475b34cbb0dcbb80b21
Kirens <dev@erik.work>
2019-01-05 14:22:34 +0100

    These conceals were not helpful

--- userspace/nvim/rc.nix
+++ userspace/nvim/rc.nix
@@ -52,23 +52,6 @@ autocmd FocusLost * stopinsert | wall!
 " map mouse activity
 set mouse=a
 
-
-let g:javascript_conceal_function = "ƒ"
-let g:javascript_conceal_null = "ø"
-let g:javascript_conceal_this = "@"
-let g:javascript_conceal_return = "⇚"
-let g:javascript_conceal_undefined = "¿"
-let g:javascript_conceal_NaN = "ℕ"
-let g:javascript_conceal_prototype = "¶"
-let g:javascript_conceal_static = "•"
-let g:javascript_conceal_super = "Ω"
-let g:javascript_conceal_arrow_function = "⇒"
-let g:javascript_conceal_noarg_arrow_function = "○"
-let g:javascript_conceal_underscore_arrow_function = "○"
-
-set conceallevel=1
-
-
 let g:solarized_termcolors = 256
 
 ''
