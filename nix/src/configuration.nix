8cf03ed1ba41c699f943fcd788e9f0284e477572
Kirens <dev@erik.work>
2019-01-05 14:11:10 +0100

    Move stateVersion to hardware config

--- nix/src/configuration.nix
+++ nix/src/configuration.nix
@@ -92,10 +92,4 @@ in
   # sound.enable = true;
   # hardware.pulseaudio.enable = true;
   # hardware.pulseaudio.support32Bit = true;
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
 }
