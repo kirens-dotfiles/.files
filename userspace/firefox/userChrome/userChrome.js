9fc2ab1c18087374aa1cb0d4694dbfc7f1b62eb2
Kirens <dev@erik.work>
2019-01-10 10:41:06 +0100

    Close tabs with Ctrl+D in Firefox

--- userspace/firefox/userChrome/userChrome.js
+++ userspace/firefox/userChrome/userChrome.js
@@ -1,15 +1,24 @@
-console.log('Running custom userChrome script')
+console.log('Running custom userChrome script, V 0.1.2')
 
-;[ { selector: '#key_quitApplication'
+;
+[ { selector: '#key_quitApplication'
   , msg: 'Will remove Ctrl+Q quit application shortcut'
   }
+, { selector: '*[key="d"][command="Browser:AddBookmarkAs"]'
+  , msg: 'Will remove Ctrl+D bookmark'
+  }
 , { selector: '*[key="W"][command="cmd_close"]'
-  , msg: 'Will remove Ctrl+W close tab shortcut'
+  , msg: 'Will remap Ctrl+W close tab shortcut to Ctrl+D'
+  , remap: 'D'
   }
 , { selector: '*[key="W"][command="cmd_closeWindow"]'
-  , msg: 'Will remove Ctrl+W close tab shortcut'
+  , msg: 'Will remove Ctrl+Shift+W close tab shortcut'
   }
-].map(({ selector, msg }) => {
+].map(({ selector, msg, remap }) => {
   console.info(msg)
-  document.querySelector(selector).remove()
+  if (remap) {
+    document.querySelector(selector).setAttribute('key', remap)
+  } else {
+    document.querySelector(selector).remove()
+  }
 })
