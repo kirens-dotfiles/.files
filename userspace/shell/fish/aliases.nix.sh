cc00ff537329a612be3ed25a735a2ee97091a381
Kirens <dev@erik.work>
2019-01-19 20:20:24 +0100

    Fish alias -> abbr
    What I'm really using is abbrevations, and now tab-completion works better. Some unused aliases removed as well

--- userspace/shell/fish/aliases.nix.sh
+++ userspace/shell/fish/aliases.nix.sh
@@ -1,70 +1,43 @@
 { }:
 ''
 # Prevent unintentional overwrites
-alias mv='mv -i'
-alias cp='cp -i'
+abbr --add mv mv -i
+abbr --add cp cp -i
 
 # TMux
 alias tmux-end-inactive="tmux ls | grep --invert-match attached | grep -oP '^[^:]+' | xargs -L 1 tmux kill-session -t"
 
-# Fish helper
-alias prefixTime='while read pong; echo [(date +%H:%M:%S)] $pong; end'
-alias prefixDatetime='while read pong; echo [(date +"%d/%m %H:%M:%S")] $pong; end'
-alias trackCon='ping -i 20 1.1.1.1 | prefixTime'
-
-alias r="vi -R"
+# View files
+abbr --add r vi -R
 
 # Git shorthands
-alias gs="git status"
-alias gsu="gs -uno"
-alias ga="git add"
-alias gc="git commit"
-alias gcm="gc -m"
-alias gcam="gc -am"
-alias gf="git fetch"
-alias gm="git merge"
-alias gp="git push"
-alias gd="git diff"
-alias gl="git log"
-alias gl1="gl -n 1"
-alias gprune="gf --prune"
-
-# LaTeX stuff
-alias mkxe='sh -c \'xelatex -halt-on-error -output-directory=./out $0 > ./out/std_out\' '
+abbr --add gs git status
+abbr --add gsu gs -uno
+abbr --add ga git add
+abbr --add gc git commit
+abbr --add gcm git commit -m
+abbr --add gcam git commit -am
+abbr --add gf git fetch
+abbr --add gm git merge
+abbr --add gp git push
+abbr --add gd git diff
+abbr --add gl git log
+abbr --add gl1 git log -n 1
+abbr --add gprune git fetch --prune
 
 # Move about and look around
-alias l="ls -lAh"
-alias la="ls -A"
-alias ld="l -d"
-alias llast='sh -c \'ls -hlt $0 | head -2\' '
-alias llasts='sh -c \'ls -hlt $1 | head -$(( 1 + $0 ))\' '
+abbr --add l ls -lAh
+abbr --add la ls -A
+abbr --add ld ls -ld
 
 # Copy paste pipe
-alias cpy=pbcopy
-alias pst=pbpaste
-
-# Sound
-alias lssound='pacmd list-sinks | grep -e device.string -e \'name:\' -e index -e sample'
-alias lsaudio='pacmd list-sources | grep -e device.string -e \'name:\' -e index'
-
-# Power mgmt
-alias hibernate-pc="i3lock-fancy& systemctl hibernate"
-alias sleep-pc="i3lock-fancy& systemctl suspend"
+abbr --add cpy pbcopy
+abbr --add pst pbpaste
 
 # Nixos shortcut
 alias nix-storefolders="find /nix/store -maxdepth 1 -type d -name '*' -a ! -name 'trash' -a ! -wholename '/nix/store' -a ! -name '.links'"
 alias nix-storefind='nix-storefolders | grep'
 
-alias nix-system="sudo nix-env -p /nix/var/nix/profiles/system"
-alias bootentry="sudo nixos-rebuild switch -p"
-
-# Battery
-alias battery="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E 'state|time\ to\ full|time\ to\ empty|percentage'"
-
-# Screenshots
-alias screenshot-last='ls -t ~/screenshots/ | head -1 | awk \'{print "/home/kiren/screenshots/" $1}\' '
-alias screenshot-view-last='screenshot-last | xargs feh'
-alias screenshot-edit-last='screenshot-last | xargs gimp'
-alias screenshot-remove-last='screenshot-last  | xargs rm'
-alias screenshot-edit-remove-last='screenshot-edit-last; screenshot-remove-last'
+abbr --add nix-system sudo nix-env -p /nix/var/nix/profiles/system
+abbr --add nix-bootentry sudo nixos-rebuild switch -p
 ''
