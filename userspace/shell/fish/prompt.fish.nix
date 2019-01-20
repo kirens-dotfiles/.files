e95c6bdb88d3f7b87aaa34fc15a95584c21f6857
Kirens <dev@erik.work>
2019-01-20 09:05:48 +0100

    Remove old shell prompt
    I will likely not use it any more

--- userspace/shell/fish/prompt.fish.nix
+++ userspace/shell/fish/prompt.fish.nix
@@ -6,29 +6,6 @@ function fish_greeting
   echo Current shell depth is (shell-lvl)
 end
 
-
-
-### Set optinos ###
-# Print full names
-set -x fish_prompt_pwd_dir_length 0
-
-
-## Git Shell stuff
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-#set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch $fish_color_param
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
-
 set -g __fish_prompt_last_lines 0
 set -g __fish_prompt_last_cols 0
 set -g __fish_prompt_last_scroll 0
@@ -73,32 +50,13 @@ function __fish_prompt_fixMargin
   end
 end
 
-# Prompt non-zero exits
-function __fish_prompt_status
-  if test "$argv[1]" -ne 0
-    printf [
-    set_color red
-    printf "$argv[1]"
-    set_color normal
-    printf '] '
-  end
-end
-
 # The function that is executed on prompt before cmd input
 function fish_prompt
-  set last_status $status
+  set -l last_status $status
 
   __fish_prompt_fixMargin
 
-  if type -q ${powerline}
-    ${powerline} --shell bare --error $last_status -max-width (${tput} cols)
-    printf '\n> '
-  else
-    # Print everything
-    echo -s (__fish_prompt_status "$last_status") "$USER" @ (prompt_hostname) \
-      ' ' (set_color $fish_color_cwd) (prompt_pwd) \
-      (__fish_git_prompt)
-    echo -n -s "> "
-  end
+  ${powerline} --shell bare --error $last_status -max-width (${tput} cols)
+  printf '\n> '
 end
 ''
