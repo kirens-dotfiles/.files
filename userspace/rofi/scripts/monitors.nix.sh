4a54a738373b7ddd9a561b508812324a5638d67a
Kirens <dev@erik.work>
2019-03-17 22:31:22 +0100

    Nix-update renamed monitors

--- userspace/rofi/scripts/monitors.nix.sh
+++ userspace/rofi/scripts/monitors.nix.sh
@@ -17,16 +17,36 @@ fi
 
 case "$res" in
     0)
-        ${xrandr} --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+        ${xrandr} \
+          --output HDMI-2 --off \
+          --output HDMI-1 --off \
+          --output DP-1 --off \
+          --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+          --output VIRTUAL-1 --off
         ;;
     1)
-        ${xrandr} --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --off --output VIRTUAL1 --off
+        ${xrandr} \
+          --output HDMI-2 --off \
+          --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+          --output DP-1 --off \
+          --output eDP-1 --off \
+          --output VIRTUAL-1 --off
         ;;
     2)
-        ${xrandr} --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output VIRTUAL1 --off
+        ${xrandr} \
+          --output HDMI-2 --off \
+          --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+          --output DP-1 --off \
+          --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal \
+          --output VIRTUAL-1 --off
         ;;
     3)
-        ${xrandr} --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+        ${xrandr} \
+          --output HDMI-2 --off \
+          --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+          --output DP-1 --off \
+          --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+          --output VIRTUAL-1 --off
         ;;
     *)
         ;;
