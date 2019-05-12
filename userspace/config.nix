42c6b50f9f331d9b2791ed8b8480ebca2b7f71b4
Erik Nygren <dev@erik.work>
2019-05-12 19:27:36 +0200

    Enable unfree excemptions based on license.
    This was a changed to allow using the xkcd font but is general enough to
    work for arbitrary future licenses.

--- userspace/config.nix
+++ userspace/config.nix
@@ -1,10 +1,42 @@
-{
+let
+  inherit (builtins)
+    parseDrvName
+    elem
+    trace
+    ;
+in {
   allowUnfreePredicate = (pkg:
-    builtins.elem
-      (builtins.parseDrvName pkg.name).name
-      [
+    let
+      unknown = "unknown";
+      pkgName = pkg.name or unknown;
+      name = if pkgName != unknown
+        then (parseDrvName pkgName).name
+        else unknown
+        ;
+
+      pkgLicense =
+        pkg.meta.license.spdxId
+        or pkg.meta.license.fullName
+        or unknown;
+
+      exemptPkgs = [
         "spotify"
         "gitkraken"
-      ]
+      ];
+      exemptLicenses = [
+        "CC-BY-NC-3.0"
+      ];
+
+      prompt = exempted:
+        if exempted
+        then trace "Exempting unfree licensed: ${name} (${pkgLicense})" true
+        else false
+        ;
+
+    in prompt (
+      elem name exemptPkgs
+      ||
+      elem pkgLicense exemptLicenses
+    )
   );
 }
