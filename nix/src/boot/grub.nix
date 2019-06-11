080970317b38472880316ee7828a09e50fbc90c7
Kirens <dev@erik.work>
2019-06-11 11:16:12 +0200

    Prefer indentation over boilerplate
    Made everything recursively instead of using long assessors. Not sure about which way to go, the best solution might be somewhere in between.

--- nix/src/boot/grub.nix
+++ nix/src/boot/grub.nix
@@ -1,28 +1,32 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "usb_storage"
-    "sd_mod"
-    "rtsx_pci_sdmmc"
-  ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "usb_storage"
+      "sd_mod"
+      "rtsx_pci_sdmmc"
+    ];
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = null;
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    device = "nodev";
-    efiSupport = true;
-    gfxmodeEfi = "1920x1080";
-    splashImage = pkgs.callPackage ./splashImage.nix {
-      message = config.myCfg.bootMessage;
+    # Use the systemd-boot EFI boot loader.
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      timeout = null;
+      grub = {
+        enable = true;
+        version = 2;
+        device = "nodev";
+        efiSupport = true;
+        gfxmodeEfi = "1920x1080";
+        splashImage = pkgs.callPackage ./splashImage.nix {
+          message = config.myCfg.bootMessage;
+        };
+      };
     };
   };
 }
