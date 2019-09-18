38d5941ca16af8eb62242f56b479498f1bf0d597
Kirens <dev@erik.work>
2019-09-18 23:24:38 +0200

    fish functions: lampa: update to adhere to new api

--- userspace/shell/fish/functions/lampa.nix.fish
+++ userspace/shell/fish/functions/lampa.nix.fish
@@ -1,6 +1,6 @@
 { curl }:
 ''
 function lampa
-  ${curl}/bin/curl "https://lampa.click/lampa?mode=rgb&time=1000&hex=$argv[1]"
+  ${curl}/bin/curl "https://lampa.click/lampa?mode=rgb&ciel&desk&duration=1000&hex=$argv[1]"
 end
 ''
